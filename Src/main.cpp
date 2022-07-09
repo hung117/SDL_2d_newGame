2e26169f7843dd835793d0799a384905d56a1f42
hung117 <snaketrex0001@gmail.com>
2022-07-09 15:03:52 +0700

    SDL_SetUp
    make file & SDL2 set up, able to create a window. nothing more

---
+++ Src/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "./include/SDL2/SDL.h"
+// #include "./include/renderWindow.h"
+// #include "./include/SDL2/SDL_image.h"
+
+using namespace std;
+
+const int WIDTH = 800, HEIGHT = 600;
+
+int main(int argc, char *argv[])
+{
+
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Window *window = SDL_CreateWindow("HELLO SDL", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
+
+    // if (window == NULL || !(IMG_Init(IMG_INIT_PNG)))
+    if (window == NULL)
+    {
+        cout << "Can't creat window" << SDL_GetError() << endl;
+        return 1;
+    }
+    SDL_Event windowEvent;
+    while (true)
+    {
+        if (SDL_PollEvent(&windowEvent))
+        {
+            if (windowEvent.type == SDL_QUIT)
+            {
+                break;
+            }
+        }
+    }
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return EXIT_SUCCESS;
+
+    return 0;
+}
---
+++ makefile
@@ -0,0 +1,29 @@
+CC=g++
+
+SDIR=./Src/include
+CFLAGS=-I$(SDIR)
+
+LDIR=-L ./Src/lib
+LIBS= -lmingw32 -lSDL2main -lSDL2 
+
+HDIR = ./Src/include
+_HEADER= renderWindow.h
+HEADER=$(patsubst %,$(HDIR)/%,$(_HEADER))
+
+SrcDIR = ./Src/
+_SRC=main.cpp
+SRC=$(patsubst %,$(SrcDIR)/%,$(_SRC))
+
+all:
+# $(CC) $(CFLAGS) $(LDIR) -o ./exe/game.exe ./Src/main.cpp $(LIBS)
+	$(CC) $(CFLAGS) $(LDIR) -o ./exe/game.exe $(SRC) $(HEADER) $(LIBS)
+
+.PHONY: clean
+clean:
+	@echo "clean project"
+	del -f *.o 
+	@echo "clean completed"
+cleanall:
+	@echo "clean exe"
+	del -f *.o *exe 
+	@echo "clean completed"*.exe
